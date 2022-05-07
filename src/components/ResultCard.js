e2dabdcd0196e4b0e593ab1e869dc058261e54fb
Ayush Iyankan <75990868+AyushIyankan@users.noreply.github.com>
2022-05-07 14:17:08 +0530

    fix Add page , source changes

---
+++ src/components/ResultCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const ResultCard = ({ movie }) => {
+  //  <img
+  //    src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+  //    alt={`${movie.title}`}
+  //  />;
+  return (
+    <div className="result-card">
+      <div className="poster-wrapper">
+        {movie.poster_path ? (
+          <img
+            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+            alt={movie.title}
+          />
+        ) : (
+          <div className="filler"></div>
+        )}
+      </div>
+
+      <div className="info">
+        <div className="header">
+          <h3 className="title">{movie.title}</h3>
+          <h4 className="release-date">
+            {movie.release_date ? movie.release_date.substring(0, 4) : "-"}
+          </h4>
+        </div>
+
+        <div className="controls">
+          <button className="btn">Add to Watchlist</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResultCard;
