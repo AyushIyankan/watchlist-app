9ea711133b18e9fa87dfadc110a2f45795838613
Ayush Iyankan <75990868+AyushIyankan@users.noreply.github.com>
2022-05-07 15:04:59 +0530

    add context

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,15 +1,29 @@
 import React, { createContext, useReducer, useEffect } from "react";
+import { AppReducer } from "./AppReducer";
 
-//initial state
+// initial state
 const initialState = {
-  watchlist: [],
+  watchList: [],
   watched: [],
 };
 
-//create context
+// create context
 export const GlobalContext = createContext(initialState);
 
-//provide components
+// provider component
 export const GlobalProvider = (props) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  //actions
+  const addMovieToWatchlist = (movie) => {
+    dispatch({ type: "ADD_MOVIE_TO_WATCHLIST", payload: movie });
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{ watchList: state.watchList, watched: state.watched }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
 };
